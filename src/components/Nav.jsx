fa02e209c9747d254603a76919942dac458e5cb6
MrLad01 <ladayodeji@gmail.com>
2023-05-05 22:29:21 +0100

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,6 +16,11 @@ const Nav = ({ userName }) => {
                 <Form
                     method="post"
                     action="/logout"
+                    onSubmit={(event) => {
+                      if (!confirm("Delete user and all data?")) {
+                        event.preventDefault()
+                      }  
+                    }}
                 >
                     <button className="btn btn--warning">
                         <span>Delete User</span>
